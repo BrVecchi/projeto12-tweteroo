52077a4107be1effcaaef53f17accee864326f2b
BrVecchi <bno.vecchi@gmail.com>
2022-11-04 16:00:14 -0300

    get only the last 10 tweets/add tweets predefined

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 import express from "express";
 import cors from "cors";
 
-const USERS = [];
-const TWEETS = [];
+const USERS = [
+  {
+    username: 'bobesponja', 
+    avatar: "https://conteudo.imguol.com.br/c/parceiros/c3/2020/06/14/bob-esponja-a-um-personagem-lgbt-confirma-nickelodeon-revelaaao-acontece-durante-mas-do-orgulho-1592150312289_v2_900x506.jpg" 
+  },
+  {
+    username: 'patrick', 
+    avatar: "https://www.torredevigilancia.com/wp-content/uploads/2020/08/patrick-star-wallpaper-810x608.jpg" 
+  }
+];
+const TWEETS = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o Patrick"
+  },
+  {
+    username: "patrick",
+    tweet: "eu amo o Bob Esponja"
+  },
+  {
+    username: "bobesponja",
+    tweet: "que d+!!!"
+  },
+  {
+    username: "patrick",
+    tweet: "salamaleico"
+  }
+];
 
 const app = express();
 app.use(cors());
@@ -34,7 +60,8 @@ app.get("/tweets", (req, res) => {
     const user = USERS.find((user) => user.username === tweet.username);
     tweet.avatar = user.avatar;
   });
-  res.status(201).send(TWEETS);
+  const newTweets = TWEETS.slice(-10)
+  res.status(201).send(newTweets);
 });
 
 app.listen(5000);
