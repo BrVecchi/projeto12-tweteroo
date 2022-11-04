6369a75d3a8378c05e012f866a94d18dfe90a252
BrVecchi <bno.vecchi@gmail.com>
2022-11-04 11:25:25 -0300

    add post method for the page /sing-up and the validation (bonus)

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import express from "express"
+import cors from "cors"
+
+const USERS = []
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.use(express.json)
+app.post("/sign-up", (req, res) => {
+    USERS.push(req.body)
+    const {username, avatar} = req.body
+    if (!username || !avatar) {
+        res.status(400).send("Todos os campos são obrigatórios!")
+        return
+    }
+    res.status(201).send("Ok")
+})
+
+
+
+
+app.listen(5000)
